bc4840492b22b4689dd8f9f75c26404e8620e0cd
Floris Kint <floris.kint@gmail.com>
2018-06-26 03:23:38 +0200

    Explicitly depend on underscore and tracker: compatible with Meteor 1.7

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
-  name: 'percolate:paginated-subscription',
+  name: 'fkint:paginated-subscription',
   summary: "Easily paginate a subscription that takes a limit parameter.",
-  version: "0.2.4",
-  git: "https://github.com/percolatestudio/paginated-subscription.git"
+  version: "0.2.5",
+  git: "https://github.com/FKint/paginated-subscription.git"
 });
 
 Package.onUse(function (api, where) {
-  api.versionsFrom("METEOR@0.9.0");
+  api.use(['underscore', 'tracker']);
   api.addFiles('paginated_subscription.js', 'client');
 });
